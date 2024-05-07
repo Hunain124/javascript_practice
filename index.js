05f2a2884aec9b540e401fef7f67cecec31855e5
hunain shahid <hunainshahid124@gmail.com>
2024-05-07 05:26:22 -0700

    pactice of conditional satatement or loops and strings methods

---
+++ index.js
@@ -0,0 +1,39 @@
+// practice of if else
+
+// let marks=prompt("Enter your marks");
+// let grade;
+
+// if(marks>=80 && marks<=100){
+//     grade="A";
+// }else if (marks>=70 && marks<=89){
+//     grade="B";
+// }else if (marks>=60 && marks<=69){
+//     grade="C";
+// }else if (marks>=50 && marks<=59){
+//     grade="D";
+// }else if (marks>=0 && marks<=49){
+//     grade="F";
+// }
+
+// console.log("According to your marks your grade was :",grade)
+
+// practice of loops
+// for(i=0; i<=100; i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+// }
+
+// let number=25;
+// let guess=prompt("Enter your Number");
+// while(guess!=number){
+//     user= prompt("Enter correct number!");
+// }
+// console.log("Congratulation you won!");
+
+// practice of strings
+
+let userName=prompt("Enter your Name");
+let userDetail= userName;
+let passName = "@" + userDetail + userDetail.length;
+console.log(passName)
