1117044eab2ccd084d817370904fcb37a11dd78b
hunain shahid <hunainshahid124@gmail.com>
2024-05-12 21:05:20 -0700

    learn arrays and arrays methods

--- index.js
+++ index.js
@@ -33,7 +33,40 @@
 
 // practice of strings
 
-let userName=prompt("Enter your Name");
-let userDetail= userName;
-let passName = "@" + userDetail + userDetail.length;
-console.log(passName)
+// let userName=prompt("Enter your Name");
+// let userDetail= userName;
+// let passName = "@" + userDetail + userDetail.length;
+// console.log(passName)
+
+// practice of arrays
+
+// question no 1
+
+// let studentMarks = [85,97,44,37,76,60,56,85,25,47];
+//  let sum=0;
+//  for (let i=0; i<studentMarks.length; i++){
+//     sum += studentMarks[i];
+//  }
+//  console.log(sum)
+
+// let average = sum / studentMarks.length;
+//  console.log(`your average marks is ${average}`)
+
+// question no 2
+
+// let items = [665,885,1025,654,999,599,449];
+// for(let i=0; i<items.length; i++){
+//     let offer = items[i]/10 ;
+//     let discountOffer = items[i] - offer;
+//     console.log(`my  final offer is = ${discountOffer}`)
+// }
+
+// practice arrays methods
+
+let campanies= ["bloomberg","microsoft","uber","Google","IBM","Netflix"]
+
+let remove =campanies.splice(2,1,"Ola")
+console.log(remove)
+let add = campanies.push("Amazone")
+console.log(add)
+console.log(campanies)
